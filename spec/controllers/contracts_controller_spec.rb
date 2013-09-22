650a691f6f525298803505e5310b915fd1ce8396
Roman Budnikov <budnikov1990@gmail.com>
2013-09-23 02:47:36 +0300

    add pagination to balance and update_contract action

--- spec/controllers/contracts_controller_spec.rb
+++ spec/controllers/contracts_controller_spec.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe ContractsController do
@@ -17,13 +18,14 @@ describe ContractsController do
   end
 
   describe 'GET balance' do
-    before{ authorization; get :balance }
+    before{ authorization; get :balance, { id: 1 } }
     context 'unauthorized' do
       let(:authorization){ nil }
       it{ response.should redirect_to new_contract_session_path }
     end
     context 'authorized' do
       it{ response.should render_template 'balance' }
+      it{ assigns(:payments).should == contract.payments.order('dt DESC').paginate(page: 1, per_page: 5)}
     end
   end
 
@@ -45,32 +47,42 @@ describe ContractsController do
       it{ response.should redirect_to new_contract_session_path }
     end
     describe 'authorized' do
-      context "invalid params" do
-        before{ authorization; post :update_contract, { sms: false, email: 'my1.mail.ru' } }
-        it{ response.should redirect_to root_path }
-        it{ contract.contract_parameter_type3.where(pid:8).limit(1).last.should == nil }
-        it{ contract.flags.where(pid:46).limit(1).last.should == nil }
+      describe "invalid params" do
+        context "email" do
+          before{ authorization; post :update_contract, { sms: 0, email: 'my1.mail.ru' } }
+          it{ response.should redirect_to root_path }
+          it{ flash[:error].should == 'Email имеет неверное значение' }
+          it{ contract.contract_parameter_type3.where(pid:8).limit(1).last.should == nil }
+        end
+        #context "sms" do
+        #  before{ authorization; post :update_contract, { sms: nil, email: 'my1@mail.ru' } }
+        #  it{ response.should redirect_to root_path }
+        #  it{ flash[:error].should == 'Val не может быть пустым' }
+        #  it{ contract.flags.where(pid:46).limit(1).last.should == 0 }
+        #end
       end
       context "valid params" do
-        before{ authorization; post :update_contract, { sms: false, email: 'my@mail.ru' } }
+        before{ authorization; post :update_contract, { sms: 0, email: 'my@mail.ru' } }
         it{ response.should redirect_to root_path }
         it{ contract.contract_parameter_type3.where(pid:8).last.email.should == 'my@mail.ru' }
         it{ contract.flags.where(pid:46).last.val.should == 0 }
-        it{ contract.contract_parameter_type3.where(pid:8).count.should == 'my@mail.ru' }
+        it{ contract.contract_parameter_type3.where(pid:8).count.should == 1 }
         it{ contract.flags.where(pid:46).count.should == 1 }
+        it{ flash[:notice].should == 'Данные обновлены' }
       end
       context "valid with existing flags and email" do
         before do
           FactoryGirl.create :email, contract: contract
           FactoryGirl.create :sms, contract: contract
           authorization
-          post :update_contract, { sms: false, email: 'my@mail.ru' }
+          post :update_contract, { sms: 0, email: 'my@mail.ru' }
         end
         it{ response.should redirect_to root_path }
         it{ contract.contract_parameter_type3.where(pid:8).last.email.should == 'my@mail.ru' }
         it{ contract.flags.where(pid:46).last.val.should == 0 }
         it{ contract.contract_parameter_type3.where(pid:8).count.should == 1 }
         it{ contract.flags.where(pid:46).count.should == 1 }
+        it{ flash[:notice].should == 'Данные обновлены' }
       end
     end
   end
