eeb78b85c0a4849a918572067a2cbeef5d766c8b
Dipress <budnikovdo@gmail.com>
2013-10-10 17:35:39 +0300

    uid

--- app/controllers/ecconnect_rails/ecconnect_payments_controller.rb
+++ app/controllers/ecconnect_rails/ecconnect_payments_controller.rb
@@ -21,7 +21,7 @@ module EcconnectRails
     def notify_right_signature_and_code ecconnect_payment
       if not ecconnect_payment.status
         contract = Contract.find ecconnect_payment.payer_id
-        payment = Payment.create! dt: Time.now, cid: contract.id, pt: 1, uid: 0, summa: ecconnect_payment.amount, lm: Time.now, comment: "UPC с карты #{ecconnect_payment.proxy_pan}"
+        payment = Payment.create! dt: Time.now, cid: contract.id, pt: 1, uid: 11, summa: ecconnect_payment.amount, lm: Time.now, comment: "UPC с карты #{ecconnect_payment.proxy_pan} код подтверждения #{ecconnect_payment.approval_code}"
         last_balance = contract.last_balance
         Balance.update_all "summa2=#{(ecconnect_payment.amount + last_balance.summa2)} where yy=#{last_balance.yy} and mm=#{last_balance.mm} and cid=#{last_balance.cid} limit 1"
         if ![0,4].include?(contract.status) && contract.balance_summa > contract.closesumma
