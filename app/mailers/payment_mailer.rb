ce32f70c6fb47cd38ebda1b3106783f7fc983299
Dipress <budnikovdo@gmail.com>
2013-12-20 10:19:50 +0200

    email title fixed

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -3,6 +3,6 @@ class PaymentMailer < ActionMailer::Base
 
   def upc_added payment, to_email
     @payment = payment
-    mail(from: "ruby.ci.ukrpack.net@crimeainfo.com", to: to_email, subject: "Поступила оплата по UPC")
+    mail(from: "ruby.ci.ukrpack.net@crimeainfo.com", to: to_email, subject: "Оплата Эквайринг-Аваль")
   end
 end
